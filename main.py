3c446a5b755aaa80dcf640801e1204441757e8ea
Jason Taylor <jtaylor@seek.com.au>
2019-03-18 10:08:45 +1100

    notes

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+~~ setup 
+
+select 'read' folder using gui
+select 'write' folder using gui
+
+print and edit mappings
+
+select file renaming settings
+
+select auto or manual start
+
+print settings and confirm, else restart
+
+walk folder and print number of files
+
+start (auto or manual based on setting above)
+
+~~ end setup
+
+~~ cleaning
+
+live percent moved counter on screen
+
+~~ end cleaning
+
+done
+"""
