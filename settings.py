0aad14c42fc7dcc3aa9d9b9886530e87c2da820d
Jason (Perry) Taylor <jtaylor@seek.com.au>
2019-10-25 13:51:44 +1100

    typo

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ from constants import *
 # File types (add new file extensions here to map them to the correct folder)
 
 SETTING_MAPPING = {
-    "documets": [
+    "documents": [
         "doc",
         "docx",
         "xls",
